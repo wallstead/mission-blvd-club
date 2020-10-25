60fe23029ff899dbd4d9f766bc7e3d4a5d721914
Willis Allstead <willis.allstead@noblestudios.com>
2020-10-25 16:46:41 -0700

    Integrate main nav links

--- wordpress/wp-content/themes/mission-blvd-club/header.php
+++ wordpress/wp-content/themes/mission-blvd-club/header.php
@@ -10,16 +10,30 @@
 <?php
 $menuBuilder = new MenuBuilder();
 $mainNav = $menuBuilder->buildNav('Main Navigation');
+$linkCount = count($mainNav);
+$logoIndex = $linkCount/2 ;
 ?>
 
 <body <?php body_class(); ?>>
     <header id="masthead" class="header js-header" role="banner">
         <a class="screen-reader-text skip-link" href="#content">Skip to content</a>
         <nav class="header__nav container">
-            <a href="#" class="header__main-nav-link">One</a>
-            <a href="#" class="header__main-nav-link">Two</a>
-            <a href="#" class="header__main-nav-link"><div class="header__logo"></div></a>
-            <a href="#" class="header__main-nav-link">Three</a>
-            <a href="#" class="header__main-nav-link">Four</a>
+            <?php if (isset($mainNav) && !empty($mainNav)) : ?>
+                <?php for ($i=0; $i < $linkCount + 1; $i++): ?>
+                    <?php if ($i == $logoIndex): ?>
+                        <div class="header__main-nav-link-container">
+                            <a href="#" class="header__main-nav-link"><div class="header__logo"></div></a>
+                        </div>
+                    <?php elseif ($i < $logoIndex): ?>
+                        <div class="header__main-nav-link-container">
+                            <a href="#" class="header__main-nav-link"><?= $mainNav[$i]->title; ?></a>
+                        </div>
+                    <?php elseif ($i > $logoIndex): ?>
+                        <div class="header__main-nav-link-container">
+                            <a href="#" class="header__main-nav-link"><?= $mainNav[$i-1]->title; ?></a>
+                        </div>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            <?php endif; ?>
         </nav>
     </header>
