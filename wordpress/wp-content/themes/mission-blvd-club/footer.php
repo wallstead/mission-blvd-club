6dd145eca60860330858d4cd9d2892c7af2d0775
Willis Allstead <willis.allstead@noblestudios.com>
2020-11-28 11:50:37 -0800

    Add ferns

--- wordpress/wp-content/themes/mission-blvd-club/footer.php
+++ wordpress/wp-content/themes/mission-blvd-club/footer.php
@@ -5,9 +5,55 @@
     ?>
 
     <footer class="footer js-footer">
-        <p>footer</p>
+        <div class="footer__container container">
+            <div class="footer__section">
+                <p class="footer__section-title">Mission Boulevard Club</p>
+                <div class="footer__section-links">
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                </div>
+            </div>
+            <div class="footer__section">
+                <p class="footer__section-title">Boulevard Heights Radio</p>
+                <div class="footer__section-links">
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                        <div class="footer__external-link-item">
+                            test
+                        </div>
+                </div>
+            </div>
+        </div>
     </footer>
-
     <?php wp_footer(); ?>
     </body>
 </html>
--- wordpress/wp-content/themes/mission-blvd-club/template-homepage.php
+++ wordpress/wp-content/themes/mission-blvd-club/template-homepage.php
@@ -6,32 +6,55 @@
     <div class="open-content">
         <div class="open-content__container container">
             <div class="open-content__constrained-container">
+                <section class="listing-hero">
+                    <div class="listing-hero__inner-container container">
+                        <div class="listing-hero__info-container">
+                            <div class="listing-hero__info">
+                                <h1 class="listing-hero__title" data-bg-title="Shows">Shows</h1>
+                                <p class="listing-hero__description">Boulevard Heights Radio dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </div>
+                        </div>
+                        <div class="listing-hero__listing">
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-3.56.11-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.50.54-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.54.16-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.55.49-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-3.56.11-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.50.54-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.54.16-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.55.49-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-3.56.11-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.50.54-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.54.16-PM.png" alt="">
+                            </div>
+                            <div class="show-card">
+                                <img class="show-card__image" src="/wp-content/uploads/2020/11/Screen-Shot-2020-11-27-at-4.55.49-PM.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <?php the_content(); ?>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
             </div>
         </div>
     </div>
